7a5a702ecf54a7995cf3d5e1bd1da32507d95554
emrhanari <114914680+emrhanari@users.noreply.github.com>
2022-10-25 23:54:10 +0300

    Add files via upload

---
+++ KutuphaneOtomasyonu/KutuphaneOtomasyonu/AnaEkran.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KutuphaneOtomasyonu
+{
+    public partial class AnaEkran : Form
+    {
+        public AnaEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KitapEklemeEkrani kitapEklemeEkrani = new KitapEklemeEkrani(this);
+            kitapEklemeEkrani.Show();
+            this.Hide();
+        }
+
+        public void kitapEkle(string kitapAdi)
+        {
+            listBox1.Items.Add(kitapAdi);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
